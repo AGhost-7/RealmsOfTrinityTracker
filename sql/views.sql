fad98c7a177cde1db3ed4efc0dcdf6048df6b158
AGhost-7 <hellomynameisjony@gmail.com>
2015-03-22 01:01:15 -0400

    Moar views!

--- sql/views.sql
+++ sql/views.sql
@@ -13,16 +13,31 @@ BEGIN
 	return area ~~ 'Secondary - Sanctuary%'
 			OR area ~~ '%Arcane Society%'
 			OR (area ~~ '%Guild%' 
-				AND area <> 'Primary - Eryndlyn - Guild of Shadows ')
+				AND area <> 'Primary - Eryndlyn - Guild of Shadows')
 			OR area ~~ '%The O Cult%';
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION is_tracked_guild(area text) RETURNS BOOLEAN
+AS $$
+BEGIN
+	return (area ~~ '%Guild%'
+				AND area <> 'Primary - Eryndlyn - Guild of Shadows')
+				OR area ~~ '%The O Cult%';
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE VIEW "Guild Activity" AS
 	SELECT * FROM snapshots WHERE is_guild(area) ORDER BY stamp;
 
+CREATE OR REPLACE VIEW "Tracked Guild Activity" AS
+	SELECT * FROM snapshots WHERE is_tracked_guild(area) ORDER BY stamp;
+	
 CREATE OR REPLACE VIEW "Guild Activity (Today)" AS 
 	SELECT * FROM "Activity (Today)" WHERE is_guild(area);
+
+CREATE OR REPLACE VIEW "Tracked Guild Activity (Today)" AS
+	SELECT * FROM "Activity (Today)" WHERE is_tracked_guild(area);
 
 CREATE OR REPLACE VIEW "Guild Account Activity" AS
 	SELECT DISTINCT ON(account_name, area) *
@@ -30,8 +45,20 @@ CREATE OR REPLACE VIEW "Guild Account Activity" AS
 		WHERE is_guild(area)
 		ORDER BY account_name;
 		
+CREATE OR REPLACE VIEW "Tracked Guild Account Activity" AS
+	SELECT DISTINCT ON(account_name, area) *
+	FROM snapshots
+		WHERE is_tracked_guild(area)
+		ORDER BY account_name;
+		
 CREATE OR REPLACE VIEW "Guild Account Activity (Today)" AS
 	SELECT DISTINCT ON(account_name, area) *
 	FROM "Activity (Today)"
 		WHERE is_guild(area)
+		ORDER BY account_name;
+		
+CREATE OR REPLACE VIEW "Tracked Guild Account Activity (Today)" AS
+	SELECT DISTINCT ON (account_name, area) *
+	FROM "Activity (Today)"
+		WHERE is_tracked_guild(area)
 		ORDER BY account_name;
